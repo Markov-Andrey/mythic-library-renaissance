64c8e33c6de3179c5837e5047fbb063366b1232b
Andre Markov <andremarkov@icloud.com>
2025-04-23 17:18:04 +0300

    add new world api

--- backend/api/worlds.py
+++ backend/api/worlds.py
@@ -1,23 +1,65 @@
-from fastapi import APIRouter
-from typing import List
-from pydantic import BaseModel
+import os
+import uuid
+from fastapi import APIRouter, UploadFile, File, Form
 from backend.db.db import get_db_connection
 
-
-class World(BaseModel):
-    id: int
-    name: str
-    cover_image_path: str
-
-
 router = APIRouter()
+STORAGE_DIR = "storage/worlds"
+os.makedirs(STORAGE_DIR, exist_ok=True)
 
 
-@router.get("/api/worlds", response_model=List[World])
+@router.get("/api/worlds")
 async def get_worlds():
     conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute("SELECT id, name, cover_image_path FROM worlds")
     rows = cursor.fetchall()
     conn.close()
-    return [World(id=row[0], name=row[1], cover_image_path=row[2]) for row in rows]
+
+    return [{"id": row[0], "name": row[1], "cover_image_path": row[2]} for row in rows]
+
+
+@router.post("/api/worlds_add")
+async def add_world(
+    name: str = Form(...),
+    short_description: str = Form(...),
+    full_description: str = Form(...),
+    visual_style: str = Form(...),
+    genre: str = Form(...),
+    tags: str = Form(...),
+    cover_image_path: UploadFile = File(...)
+):
+    ext = os.path.splitext(cover_image_path.filename)[1]
+    filename = f"{uuid.uuid4().hex}{ext}"
+    file_path = os.path.join(STORAGE_DIR, filename)
+
+    with open(file_path, "wb") as buffer:
+        content = await cover_image_path.read()
+        buffer.write(content)
+
+    db_path = f"/{file_path.replace(os.sep, '/')}"
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        INSERT INTO worlds (
+            name, short_description, full_description, 
+            visual_style, genre, tags, cover_image_path
+        ) 
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        """,
+        (
+            name,
+            short_description,
+            full_description,
+            visual_style,
+            genre,
+            tags,
+            db_path
+        )
+    )
+    conn.commit()
+    conn.close()
+
+    return {"message": "World add"}
--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="min-h-screen bg-gray-50 text-gray-800">
-    <div class="w-full max-w-7xl px-6 py-10 mx-auto">
-      <h1 class="text-4xl font-bold text-gray-900 mb-8">Библиотека миров</h1>
+    <div class="w-full max-w-7xl px-2 py-2 mx-auto">
+      <h1 class="text-4xl font-bold text-gray-900">Worlds Library</h1>
+      <hr class="p-1">
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
 
         <WorldCard
@@ -10,13 +11,7 @@
           :world="world"
         />
 
-        <div class="bg-gray-100 p-6 rounded-xl shadow-md hover:shadow-lg transition-all flex items-center justify-center cursor-pointer hover:brightness-125 group">
-          <div class="rounded-full bg-blue-300 w-20 h-20 flex items-center justify-center group-hover:bg-blue-400 transition-colors">
-            <svg class="w-12 h-12 text-white group-hover:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
-              <path d="M12 5v14M5 12h14"></path>
-            </svg>
-          </div>
-        </div>
+        <WorldAdd/>
 
       </div>
     </div>
@@ -27,6 +22,7 @@
 import {onMounted, ref} from 'vue';
 import ky from 'ky';
 import WorldCard from '../components/WorldCard.vue';
+import WorldAdd from '../components/WorldAdd.vue';
 
 const worlds = ref([]);
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
