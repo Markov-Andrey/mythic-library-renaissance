3fe7c64fc43a286757bf831ee9c00e4b60beae3c
Andre Markov <andremarkov@icloud.com>
2025-04-23 14:20:54 +0300

    upd world card

--- frontend/src/components/WorldCard.vue
+++ frontend/src/components/WorldCard.vue
@@ -1,15 +1,15 @@
 <template>
   <div
-    class="flex flex-col h-full bg-gray-100 rounded-xl shadow-md hover:shadow-lg transition-all cursor-pointer"
+    class="flex flex-col h-full bg-gray-100 hover:brightness-125 rounded-xl shadow-md hover:shadow-lg transition-all cursor-pointer"
     @click="goToWorldDetails"
   >
     <img
       :src="world.cover_image_path"
       alt="World Cover"
-      class="w-full h-56 object-cover rounded-xl mb-4"
+      class="w-full h-56 object-cover rounded-xl"
     />
-    <div class="rounded-xl">
-      <h2 class="text-xl font-semibold mb-4">{{ world.name }}</h2>
+    <div class="rounded-xl flex justify-center my-4">
+      <h2 class="text-xl font-semibold">{{ world.name }}</h2>
     </div>
   </div>
 </template>
--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -10,7 +10,7 @@
           :world="world"
         />
 
-        <div class="bg-gray-100 p-6 rounded-xl shadow-md hover:shadow-lg transition-all flex items-center justify-center cursor-pointer hover:bg-gray-200 group">
+        <div class="bg-gray-100 p-6 rounded-xl shadow-md hover:shadow-lg transition-all flex items-center justify-center cursor-pointer hover:brightness-125 group">
           <div class="rounded-full bg-blue-300 w-20 h-20 flex items-center justify-center group-hover:bg-blue-400 transition-colors">
             <svg class="w-12 h-12 text-white group-hover:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
               <path d="M12 5v14M5 12h14"></path>
